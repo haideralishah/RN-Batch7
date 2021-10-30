775aa27d3c0a0eeeeed8352503f695da257e0e26
haideralishah <headeralishah@gmail.com>
2021-10-29 21:19:45 -0700

    Hello world

--- App.js
+++ App.js
@@ -5,11 +5,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native on Web</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
